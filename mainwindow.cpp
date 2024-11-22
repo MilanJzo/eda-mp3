c8fd2086110b5582cbd694224a5349dc8181798a
Modryo <markus.heming@mailbox.org>
2024-11-22 19:18:22 +0100

    First steps
    - some buttons
    - import file (not finished)
    - some other stuff

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,57 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    M_Player = new QMediaPlayer();
+
+    ui->pushButton_Play->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    ui->pushButton_Pause->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+
+    audioOutput = new QAudioOutput();
+    // vol = ui->horizontalSlider_VC->value() / 100.0;
+    // audioOutput = setVolume(vol);
+    // M_Player->setAudioOuput(audioOutput);
+
+
+    //Open File: manuelle Action Zuweisung zum Button
+    actionOpen_File = new QAction(tr("Open File"),this);
+
+    connect(actionOpen_File,&QAction::triggered,this, &MainWindow::on_actionOpen_File_triggered);
+    connect(ui->pushButton_Open_File, &QPushButton::clicked, actionOpen_File, &QAction::trigger);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionOpen_File_triggered()
+{
+    QString FileName = QFileDialog::getOpenFileName(this,tr("Select Audio File"),"",tr("MP3 Files (*.MP3)"));
+    M_Player->setSource(QUrl::fromLocalFile(FileName));
+    QFileInfo File(FileName);
+    ui->label_Value_File_Name->setText(File.fileName());
+}
+
+void MainWindow::on_pushButton_Play_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_Pause_clicked()
+{
+
+}
+
+
+
+
+
+void MainWindow::on_horizontalSlider_valueChanged(int value)
+{
+
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,11 @@
 
 #include <QMainWindow>
 
+#include <QtMultimedia>
+#include <QFileDialog>
+#include <QStyle>
+
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -17,7 +22,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_Play_clicked();
+
+    void on_pushButton_Pause_clicked();
+
+    void on_actionOpen_File_triggered();
+
+    void on_horizontalSlider_valueChanged(int value);
+
 private:
     Ui::MainWindow *ui;
+    QMediaPlayer *M_Player;
+    QAudioOutput *audioOutput;
+    qfloat16 vol;
+
+    //Open File: manuelle Action Zuweisung zum Button
+    QAction *actionOpen_File;
+
 };
 #endif // MAINWINDOW_H
