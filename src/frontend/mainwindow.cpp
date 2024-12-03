8d86cf81c333cbcacc99f39e03c3cb30d75977ae
MilanJzo <milan.jezovsek@gmail.com>
2024-12-03 15:56:36 +0100

    controls widget progress
    - failed at custom svg icons
    - built ui

--- src/frontend/mainwindow.cpp
+++ src/frontend/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "controls.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->controlsHolder->layout()->addWidget(new controls(this));
 }
 
 MainWindow::~MainWindow()
